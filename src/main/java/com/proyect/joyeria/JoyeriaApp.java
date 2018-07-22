cbc279c78d19b174e11edc6a491999c691f0724e
carlos ivan velazquez <ivan.velazquez1810@outlook.com>
2018-07-22 02:25:47 -0500

    commit 1

---
+++ src/main/java/com/proyect/joyeria/JoyeriaApp.java
@@ -0,0 +1,15 @@
+package com.proyect.joyeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyeriaApp {
+
+		public static void main(String[] args){
+			SpringApplication.run(JoyeriaApp.class, args);
+			
+			
+		}
+	
+}
